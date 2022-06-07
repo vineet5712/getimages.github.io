36c6a0ee605789ad092036a80389a38c280d9624
Vineet Malhotra <63547962+vineet571@users.noreply.github.com>
2022-06-07 23:35:55 +0530

    ccs

--- blog/settings.py
+++ blog/settings.py
@@ -126,6 +126,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = os.patha.join(BASE_DIR,'staticfiles')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
